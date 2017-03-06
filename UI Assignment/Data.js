20bbff151166710fbc82a2c25cb53be702a3ed67
irfan <irfan.syed.ap@nielsen.com>
2017-03-06 20:12:07 +0530

    Initial Commit

---
+++ UI Assignment/Data.js
@@ -0,0 +1,31 @@
+var dataObject ={
+		"question1":{
+			"question":"When was the last time India won the Cricket World Cup?",
+			"options":["1999","2003","2011","2015"],
+			"correctOptions":"2011"
+						
+		},
+		"question2":{
+			"question":"What is the highest individual score by a batsman in Test Cricket?",
+			"options":["375","400","303","390"],
+			"correctOptions":"400"
+						
+		},
+		"question3":{
+			"question":"Who won the most number of Cricket World Cups?",
+			"options":["India","England","Australia","Pakistan"],
+			"correctOptions":"Australia"
+						
+		},
+		"question4":{
+			"question":"How many International centuries does Sachin Tendulkar has under his name?",
+			"options":["100","80","60","49"],
+			"correctOptions":"49"
+						
+		}
+		
+		
+};
+console.log(dataObject.question1.question);
+console.log(dataObject.question1.options);
+console.log(dataObject.question1.correctOptions);
