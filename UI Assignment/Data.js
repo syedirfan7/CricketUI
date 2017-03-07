f0274bb62b087df28a458ed70cf5fd95ebe3e344
irfan <irfan.syed.ap@nielsen.com>
2017-03-07 20:38:30 +0530

    added

--- UI Assignment/Data.js
+++ UI Assignment/Data.js
@@ -1,23 +1,28 @@
-var dataObject ={
-		"question1":{
+
+var app=angular.module('quiz',[]);
+
+
+app.controller('quizController',['$scope',function($scope){
+	var dataObject =[
+		{
 			"question":"When was the last time India won the Cricket World Cup?",
 			"options":["1999","2003","2011","2015"],
 			"correctOptions":"2011"
 						
 		},
-		"question2":{
+		{
 			"question":"What is the highest individual score by a batsman in Test Cricket?",
 			"options":["375","400","303","390"],
 			"correctOptions":"400"
 						
 		},
-		"question3":{
+		{
 			"question":"Who won the most number of Cricket World Cups?",
 			"options":["India","England","Australia","Pakistan"],
 			"correctOptions":"Australia"
 						
 		},
-		"question4":{
+		{
 			"question":"How many International centuries does Sachin Tendulkar has under his name?",
 			"options":["100","80","60","49"],
 			"correctOptions":"49"
@@ -25,7 +30,12 @@ var dataObject ={
 		}
 		
 		
-};
-console.log(dataObject.question1.question);
-console.log(dataObject.question1.options);
-console.log(dataObject.question1.correctOptions);
+];
+	$scope.data=dataObject;
+	
+	/* console.log(dataObject[0].question1.question);
+console.log(dataObject[0].question1.options);
+console.log(dataObject[0].question1.correctOptions); */
+}]);
+
+
